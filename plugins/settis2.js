4d5ccd1dbeedada7730cc5640ed8a8a6bdb094ca
Sila-Md <hackersila2@gmail.com>
2026-05-08 20:21:08 +0300

    SILA

--- plugins/settis2.js
+++ plugins/settis2.js
@@ -16,15 +16,15 @@ if (!fs.existsSync(SETTINGS_DIR)) {
 // Default settings
 const DEFAULT_SETTINGS = {
     // Owner settings
-    ownerName: 'Sila Tech',
-    ownerNumber: '255789661031',
+    ownerName: '𝐓𝐘𝐑𝐄𝐗 𝐌𝐃',
+    ownerNumber: '255628378557',
     
     // Sticker settings
-    stickerAuthor: 'Sila MD',
+    stickerAuthor: '𝐓𝐘𝐑𝐄𝐗 𝐌𝐃',
     stickerPackName: 'Sila Stickers',
     
     // Watermark settings
-    watermark: '© 𝐒𝐈𝐋𝐀 𝐌𝐃',
+    watermark: '© 𝐓𝐘𝐑𝐄𝐗 𝐌𝐃,
     
     // Timezone settings
     timezone: 'Africa/Dar_es_Salaam',
@@ -34,7 +34,7 @@ const DEFAULT_SETTINGS = {
     botVersion: '1.0.0',
     
     // Menu settings
-    menuImage: 'https://files.catbox.moe/36vahk.png',
+    menuImage: 'https://i.ibb.co/2YRqb2Md/upload-1777244568390-9cc80c1a-jpg.jpg',
     
     // Message settings
     welcomeMessage: 'Welcome to the group!',
@@ -133,7 +133,7 @@ const fkontak = {
         "id": "Halo"
     },
     "message": {
-        "conversation": "𝐒𝐈𝐋𝐀 𝐌𝐃"
+        "conversation": "𝐓𝐘𝐑𝐄𝐗 𝐌𝐃"
     }
 };
 
@@ -143,8 +143,8 @@ const getContextInfo = (m) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363402325089913@newsletter',
-            newsletterName: '𝐒𝐈𝐋𝐀 𝐌𝐃',
+            newsletterJid: '120363424973782944@newsletter',
+            newsletterName: '𝐓𝐘𝐑𝐄𝐗 𝐌𝐃',
             serverMessageId: 143,
         },
     };
